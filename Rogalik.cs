f16e19fc2e98038e258b7590584e9e7dcd2d9aac
neavailable <raizenradeonovich23@gmail.com>
2025-02-26 22:30:53 +0100

    complete last task

---
+++ Rogalik.cs
@@ -0,0 +1,84 @@
+using System;
+
+class Rogalik
+{
+    private static char playerChar = '$';
+    private static int sizeX = 45, sizeY = 10;  
+    private static string[] map = new string[10] 
+    {
+        "[===========================================]",
+        "[          ###                   # #        ]",
+        "[          ###                  #   #       ]",
+        "[          ###                 #     #      ]",
+        "[          ###                 #######      ]",
+        "[       ###    ###                  ##      ]",
+        "[      #   #  #   #                 ##      ]",
+        "[       ###    ###                          ]",
+        "[                                           ]",
+        "[===========================================]",
+    };
+    private static int x, y;
+    
+    private static void Main() 
+    {
+        CreatePlayer();
+        
+        string userInput;
+        do
+        {           
+            Console.Clear();
+            PrintMap();
+            userInput = Console.ReadLine();
+            InputHandling(userInput);
+        } while (userInput != "exit");
+
+    }
+    
+    private static void SetCharInMap(char character, int x_, int y_)
+    {
+        char[] row = map[y_].ToCharArray();
+        row[x_] = character;
+        map[y_] = new string(row);
+    }
+        
+    private static void InputHandling(string input)
+    {
+        int previuosX = x, previuosY = y;
+        
+        if (input == "w") --y;
+        
+        else if (input == "a") --x;
+
+        else if (input == "s") ++y;
+        
+        else if (input == "d")++x;
+        
+        
+        if (map[y][x] != ' ') 
+        {
+            x = previuosX; y = previuosY;
+            return;
+        }
+        
+        SetCharInMap(playerChar, x, y);
+        SetCharInMap(' ', previuosX, previuosY);
+    }
+    
+    private static void CreatePlayer()
+    {
+        Random random = new Random();
+        
+        do
+        {
+            x = random.Next(0, sizeX);
+            y = random.Next(0, sizeY);
+        } while (map[y][x] != ' ');
+        
+        SetCharInMap(playerChar, x, y);
+    }
+    
+    private static void PrintMap()
+    {
+        foreach (string element in map) Console.WriteLine(element);
+    }
+}
